a390186939fd2e795ea27da55871de94b19a1ad6
Borja Esteban <boressan@outlook.com>
2025-03-29 09:38:04 +0100

    feat: enhance raster and signal plotting functions with additional parameters and improved layout

--- plots.py
+++ plots.py
@@ -1,26 +1,36 @@
 import matplotlib.pyplot as plt
-import torch
+import numpy as np
 
 
-def raster(spikes, title="Raster Plot"):
+def raster(spikes, title, figsize=(12, 8), **kwds):
     """Plot the raster plot of the spikes."""
-    plt.figure()
-    for neuron_idx in range(spikes.shape[1]):
-        spike_times = torch.nonzero(spikes[:, neuron_idx]).squeeze()
-        plt.scatter(spike_times, neuron_idx * torch.ones_like(spike_times), s=1)
+    num_time_steps, num_neurons = spikes.shape
+    time_indices, neuron_indices = np.where(spikes == 1)
+
+    plt.figure(figsize=figsize)
     plt.title(title)
-    plt.xlabel("Time")
-    plt.ylabel("Neuron Index")
-    plt.xlim(0, spikes.shape[0])
-    plt.ylim(0, spikes.shape[1])
+    plt.plot(time_indices, neuron_indices, ".", **kwds)
+
+    plt.ylabel("Neuron index")
+    plt.yticks(np.linspace(0, num_neurons, min(11, num_neurons), dtype=int))
+
+    plt.xlabel("Time step")
+    plt.xlim(0, num_time_steps)
+
+    plt.grid(axis="x", linestyle="--", alpha=0.5)
+    plt.tight_layout()
     plt.show()
 
 
-def signal(output, title="Firing Rates"):
+def signal(output, title, figsize=(12, 8), **kwds):
     """Plot the signal output."""
-    plt.figure()
-    plt.plot(output)
+    num_time_steps, _num_neurons = output.shape
+    time_steps = np.arange(num_time_steps)
+
+    plt.figure(figsize=figsize)
     plt.title(title)
-    plt.xlabel("Time")
+    plt.plot(time_steps, output, "--", **kwds)
+
     plt.ylabel("Firing Rate")
+    plt.xlabel("Time")
     plt.show()
