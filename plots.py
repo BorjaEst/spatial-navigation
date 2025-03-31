fe230ec786d71a83034115c487d116f518bc10cf
Borja Esteban <boressan@outlook.com>
2025-03-31 13:09:17 +0200

    feat: update balanced_network configuration with increased population sizes and adjusted synapse parameters for excitatory and inhibitory layers; refactor plotting functions to remove title parameter

--- plots.py
+++ plots.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def raster(spikes, title, figsize=(12, 4), **kwds):
+def raster(spikes, figsize=(12, 4), **kwds):
     """Plot the raster plot of the spikes."""
     num_time_steps, num_neurons = spikes.shape
     time_indices, neuron_indices = np.where(spikes == 1)
 
     plt.figure(figsize=figsize)
-    plt.title(title)
     plt.plot(time_indices, neuron_indices, ".", **kwds)
 
     plt.ylabel("Neuron index")
     plt.yticks(np.linspace(0, num_neurons, min(11, num_neurons), dtype=int))
+    plt.ylim(0, num_neurons)
 
     plt.xlabel("Time step")
     plt.xlim(0, num_time_steps)
@@ -21,28 +21,27 @@ def raster(spikes, title, figsize=(12, 4), **kwds):
     plt.tight_layout()
 
 
-def signal(output, title, figsize=(12, 4), **kwds):
+def signal(output, figsize=(12, 4), **kwds):
     """Plot the signal output."""
     num_time_steps, _num_neurons = output.shape
     time_steps = np.arange(num_time_steps)
 
     plt.figure(figsize=figsize)
-    plt.title(title)
     plt.plot(time_steps, output, "--", **kwds)
 
     plt.ylabel("Firing Rate")
     plt.xlabel("Time")
+    plt.xlim(0, num_time_steps)
 
     plt.grid(axis="x", linestyle="--", alpha=0.5)
     plt.tight_layout()
 
 
-def weights(weights, title, figsize=(12, 4), **kwds):
+def weights(weights, figsize=(12, 4), **kwds):
     """Plot the weights."""
 
     plt.figure(figsize=figsize)
-    plt.title(title)
-    plt.hist(weights.flatten(), 50, range=[0, 10], **kwds)
+    plt.hist(weights.flatten(), 50, **kwds)
 
     plt.ylabel("Weight")
     plt.xlabel("Weight Value")
